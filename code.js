b225cf4e58a4fe22573e8577912828de09a9afcd
Grey Stag <fritz.then.fenske@gmail.com>
2022-12-20 14:34:58 -0500

    Making game repeatable and fine tuning

--- code.js
+++ code.js
@@ -2,18 +2,20 @@
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
+const classAdd = document.getElementById('rockPaperScissors')
 
-const button = document.querySelector('buttonRemove')
 const playerContent = document.querySelector('.playContent')
 const computerContent = document.querySelector('.compContent')
 const roundContent = document.querySelector('.round-winner')
 const playRounds = document.querySelector('.playRounds')
-const newGame = document.querySelector('#game')
+
 
 rock.addEventListener('click', () => playGame("rock"));
 paper.addEventListener('click', () => playGame("paper"));
 scissors.addEventListener('click', () => playGame('scissors'));
 
+const game = document.getElementById('game');
+const freshGame = game.cloneNode(true);
 
 
 
@@ -102,73 +104,42 @@ function playGame(playerInput){
 }
 
 function startNewGame() {
-    let buttons = document.getElementById('btnId')
-    while (buttons.firstChild) {
-        buttons.removeChild(buttons.firstChild);
-    }
+    
+    
+    classAdd.setAttribute('class', 'hider')
+    $('.btn').click(function(){
+        $(".hider").hide();
+    });
+    const buttons = document.getElementById('btnId');
+    const newDiv = document.createElement('div');
+    newDiv.setAttribute('id','playAgain')
+    buttons.append(newDiv);
+    
     const paragraph = document.createElement('button');
+
     paragraph.setAttribute('style', 'background: rgb(122, 120, 134); width: 300px; height: 200px; border-radius: 25px; color: aliceblue; font-size: 32px;')
     paragraph.textContent = "Play again?"
-    buttons.appendChild(paragraph)
-
-
+    newDiv.appendChild(paragraph);
+    paragraph.addEventListener('click', () => newgame('yes'));
+    
 }
 
-// let numRounds = prompt("Welcome to Rock Paper Scissors, How many rounds would you like to play?")
-// let playersInput = prompt("What do you choose, Rock, Paper or Scissors?");
-// let playersDecision = playersInput.toLowerCase();
-// console.log(playersDecision);
+function newgame(play) {
+    if (play === "yes"){
+        console.log("yes");
+            $('#playAgain').remove();
+            $('.hider').show();
+            classAdd.removeAttribute('class', 'hider');
+            
+        }
+    compWins = 0
+    playerWins = 0
+    lives = 5
+    playRounds.textContent = "You have " + lives + " lives, what weapon do you choose?"
+    computerContent.textContent = "Computer Score: "
+    playerContent.textContent = "Player Score: "
+    roundContent.textContent = "Last Round: "
+    console.log(compWins, playerWins, lives)
+}
 
-// function computersChoice(){
-//     let computerInput = Math.floor(Math.random() * 3);
-//     if (computerInput == 0) {
-//         return ("rock")
-//     }
-//     else if (computerInput == 1) {
-//         return ("paper")
-//     }
-//     else {
-//         return ("scissors")
-//     }
-// };
-// computersChoice()
-
-// let compChoice = computersChoice()
-// console.log(compChoice);
-
-// function winnerWinnerChickenDinner (compChoice, playersDecision, numRounds){ 
-//     let compWins = 0
-//     let playerWins = 0
-//     for (i = 1; i < numRounds; i++){
-//         let playersInput = prompt("What do you choose, Rock, Paper or Scissors?");
-//         let playersDecision = playersInput.toLowerCase();
-        
-//         if (playersDecision == "rock" && compChoice == "scissors") {
-//             playerWins++; console.log("playerwins" + playerWins)}
-//         else if (playersDecision == "paper" && compChoice == "rock") {
-//             playerWins++; console.log("playerwins" + playerWins)}
-//         else if (playersDecision == "scissors" && compChoice == "paper") {
-//             playerWins++; console.log("playerwins" + playerWins)}
-        
-//         else if (playersDecision == "paper"&& compChoice == "scissors"){
-//             compWins++; console.log("compwins" + compWins)}
-//         else if (playersDecision == "rock" && compChoice == "paper"){
-//             compWins++; console.log("compwins" + compWins)}
-//         else if (playersDecision == "scissors" && compChoice == "rock") {
-//             compWins++; console.log("compwins" + compWins)}	
-//         else {compWins++; playerWins++; console.log("Compwins" + compWins); console.log("playerwins" + playerWins)}
-//     }
-//     console.log(compWins)
-//     console.log(playerWins)
-//     if (compWins > playerWins) {
-//         alert("The Computer Won! Maybe next time you'll succeed.. maybe..")
-//     }
-//     else if (compWins < playerWins) {
-//         alert("You Won! Great job winning a game of chance that doesn't really take much skill...")
-//     }
-//     else {
-//         alert("Tie! Guess both of you are having a bad day.")
-//     }
-// };
-// winnerWinnerChickenDinner(compChoice, playersDecision, numRounds)
 
